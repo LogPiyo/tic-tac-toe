99e6c1152dbb2a7bf4739ce625c861df52a36aca
LogPiyo <shushu20011031@gmail.com>
2025-07-01 22:07:31 +0900

    fix: バグ修正

--- src/App.js
+++ src/App.js
@@ -59,7 +59,7 @@ function Board({ xIsNext, squares, onPlay, move }) {
 export default function Game() {
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [currentMove, setCurrentMove] = useState(0);
-  const xIsNext = history.length % 2 === 0;
+  const xIsNext = currentMove % 2 === 0;
   const currentSquares = history[currentMove];
 
   function handlePlay(nextSquares) {
