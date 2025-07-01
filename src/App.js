eea47429b514bf36a4ab6f70039aa6d603bf4b58
LogPiyo <shushu20011031@gmail.com>
2025-07-01 22:40:05 +0900

    fix: ハードコーディングを修正

--- src/App.js
+++ src/App.js
@@ -37,21 +37,14 @@ function Board({ xIsNext, squares, onPlay, move }) {
   return (
     <>
       <div className="status"> { status } </div>
-      <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleclick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleclick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleclick(2)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleclick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleclick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleclick(5)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleclick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleclick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleclick(8)} />
-      </div>
+      {Array(3).fill(null).map((_, row) => (
+        <div className="board-row" key={row}>
+          {Array(3).fill(null).map((_, col) => {
+            const index = row * 3 + col;
+            return <Square key={index} value={squares[index]} onSquareClick={() => handleclick(index)} />
+          })}
+        </div>
+      ))}
     </>
   );
 }
